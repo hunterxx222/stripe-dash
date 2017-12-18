a361e931d355d506468a7b26a52b48827119e251
Toan Ngo <xhunter9x@gmail.com>
2017-12-18 15:16:30 +0700

    done charges

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,16 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import _ from "lodash";
 
 export default class Checkout extends Component {
   constructor(props){
     super(props);
     this.state = {
-      latestCharge: 'None'
-    }
-    this.createCharge = this.createCharge.bind(this)
+      latestCharge: "None"
+    };
+    this.createCharge = this.createCharge.bind(this);
   }
 
   createCharge(){
+    const key = "pk_test_Hzx5O145IgW6uzxtl1gVAJEH";
+    const secretKey = "sk_test_BQokikJOvBiI2HlWgH4olfQ2";
 
+    const request = (route, key, method, postData) => {
+      const dataStr = (method === "GET") ? null : _.toPairs(postData).map((a) =>{
+        return `${a[0]}=${a[1]}`;
+      }).join("&");
+
+      return fetch(`https://api.stripe.com/v1/${route}`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${key}`,
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: dataStr
+      })
+        .then((data) => data.json());
+    };
+
+    this.setState({
+      latestCharge: "Creating token..."
+    }, ()=> {
+      return request("tokens", secretKey, "POST", {
+        "card[number]":"4242424242424242",
+        "card[exp_month]":"12",
+        "card[exp_year]":"2018"
+      })
+        .then((token) => {
+          this.setState({
+            latestCharge: "Creating charge..."
+          });
+          return request("charges", secretKey, "POST", {
+            "amount": 2000,
+            "currency": "usd",
+            "description": "test charge",
+            "source": token.id
+          });
+        })
+        .then((charge)=>{
+          this.setState({
+            latestCharge: charge.id
+          });
+        });
+    });
   }
 
   render() {
